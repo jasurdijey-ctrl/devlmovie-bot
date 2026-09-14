1a794e3cfdfc049058af3c88e4f5c6da18837141
jasurdijey-ctrl <jasurdijey@gmail.com>
2026-09-14 13:39:54 +0500

    Update js.js

--- js.js
+++ js.js
@@ -34,7 +34,7 @@ const bot = new Telegraf(BOT_TOKEN);
 const movies = {
   "1": { title: "Qasoskorlar: Intihoy", file_id: "BAACAgIAAxkBAPyaqeA3qLG2VubWF1Lwyx36RKS2SEAAsOgAAJgoEBJv7wYX02X4GQ9BA" },
   "2": { title: "Ferdinand multfilmi", file_id: "BAACAgIAAxkBAAIBjGqnlyQ9xPPIDZPJPwI7k6LbYCGiAAK0oQACYKBASabLP9GxaMccPQQ" },
-  "3": { title: "Uyda yolgiz 1", file_id: "BAACAgIAAxkBAAMXaqbNpPiGvhqmEXUaj1raK_m8h7cAAn2rAAJgoDhJYTsBK8W2c8Y9BA" },
+  "3": { title: "Uyda yolgiz 1", file_id: "BAACAgIAAxkBAAIB4mqnslmXGBMOwI0ZE0CLLE8CMCUUAAL4ogACYKBASWebCYjIU02oPQQ" },
   "4": { title: "Uyda yolgiz 2", file_id: "FILE_ID_YAZILADI" },
   "5": { title: "Forsaj 10", file_id: "FILE_ID_YAZILADI" },
   "6": { title: "Garri Poter 1", file_id: "FILE_ID_YAZILADI" },
